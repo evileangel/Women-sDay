d443cef92a9a020cdf694f04916aceea0ab68dc9
evileangel <150454461+evileangel@users.noreply.github.com>
2024-03-04 23:26:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'Mấy cô đích thực là Mễ Nhơn!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
